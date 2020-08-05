f0eb9462240b6de7802b55afe2089737343036e3
Guilherme Lellis <lguilherme44@gmail.com>
2020-08-04 22:26:28 -0300

    proffy - nlw#2

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// global styles
+import './assets/styles/global.css';
+
+// panding page
+import Landing from './pages/Landing';
+
+function App() {
+  return <Landing />;
+}
+
+export default App;
